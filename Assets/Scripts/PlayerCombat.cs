2e02264b63327eebc53d03ca232cab37df7e57e1
Onur Güler <guleronurosman@gmail.com>
2024-03-16 19:54:13 +0100

    Added attack animation

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -2,13 +2,13 @@ using StarterAssets;
 using UnityEngine;
 
 public class PlayerAnimatorParameters {
+    public const string ATTACK = "Attack";
+    public const string MOVE = "Move";
     public const string DRAW_WEAPON = "DrawWeapon";
     public const string SHEATH_WEAPON = "SheathWeapon";
 }
 
 public class PlayerCombat : MonoBehaviour {
-
-    public bool CanAttack => WeaponDrawed && !Attacking;
 
     public bool WeaponDrawed;
     public bool Attacking;
@@ -18,17 +18,45 @@ public class PlayerCombat : MonoBehaviour {
 
     StarterAssetsInputs _input;
     Animator _animator;
+    ThirdPersonController _thirdPersonController;
 
+    float _timePassed;
+    float _clipLength;
+    float _clipSpeed;
 
     void Start() {
         _input = GetComponent<StarterAssetsInputs>();
         _animator = GetComponent<Animator>();
+        _thirdPersonController = GetComponent<ThirdPersonController>();
     }
 
     void Update() {
         if (!Attacking && _input.drawWeapon) {
             DrawWeapon();
         }
+
+        if (_input.attack && WeaponDrawed && !Attacking) {
+            Attack();
+        }
+
+        if (Attacking) {
+            _timePassed += Time.deltaTime;
+            _clipLength = _animator.GetCurrentAnimatorClipInfo(1)[0].clip.length;
+            _clipSpeed = _animator.GetCurrentAnimatorStateInfo(1).speed;
+
+            //if (_timePassed >= _clipLength / _clipSpeed && _input.attack) {
+            //    Attack();
+            //}
+
+            if (_timePassed >= _clipLength / _clipSpeed) {
+                _animator.applyRootMotion = false;
+                Attacking = false;
+                _animator.SetTrigger(PlayerAnimatorParameters.MOVE);
+                _thirdPersonController.canMove = true;
+            }
+        }
+
+        _input.attack = false;
     }
 
     void DrawWeapon() {
@@ -42,6 +70,15 @@ public class PlayerCombat : MonoBehaviour {
         else {
             _animator.SetTrigger(PlayerAnimatorParameters.SHEATH_WEAPON);
         }
+    }
+
+    void Attack() {
+        _input.attack = false;
+        Attacking = true;
+        _animator.applyRootMotion = true;
+        _timePassed = 0f;
+        _animator.SetTrigger(PlayerAnimatorParameters.ATTACK);
+        _thirdPersonController.canMove = false;
     }
 
     #region Animation Events
