1b442b890f82dbf33d76c09e1f7c16a520fcda8a
Egemen Azay <egemenazay39@gmail.com>
2024-03-17 17:28:38 +0300

    Oyunun ilerleyişini tamamladım

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,12 +5,19 @@ public class Portal : MonoBehaviour {
     public Transform Destination;
 
     GameObject _player;
-
+    [SerializeField] private GameObject _npcs1;
+    [SerializeField] private GameObject _priest;
     void Start() {
         _player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void Teleport() {
+    void Teleport1() {
+        CharacterController characterController = _player.GetComponent<CharacterController>();
+        characterController.enabled = false;
+        _player.transform.position = new Vector3(Destination.position.x, Destination.position.y, Destination.position.z);
+        characterController.enabled = true;
+    }
+    void Teleport2() {
         CharacterController characterController = _player.GetComponent<CharacterController>();
         characterController.enabled = false;
         _player.transform.position = new Vector3(Destination.position.x, Destination.position.y, Destination.position.z);
@@ -19,7 +26,9 @@ public class Portal : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
-            Teleport();
+            Teleport1();
+            _npcs1.SetActive(false);
+            _priest.SetActive(true);
         }
     }
 
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -5,7 +5,9 @@ public class PowerUp : MonoBehaviour
 {
     [SerializeField] public Player player;
     [SerializeField] public int id;
-    private int speedamount = 5;
+    [SerializeField] public GameObject wall1;
+    [SerializeField] public GameObject wall2;
+    private int speedamount = 2;
     private int hpamount = 100;
     private int attackamount = 20;
     // Start is called before the first frame update
@@ -26,11 +28,13 @@ public class PowerUp : MonoBehaviour
             else if (id == 1)
             {
                 player.hp += hpamount;
+                wall2.SetActive(false);
             }
             else if (id == 2)
             {
                 Weapon weapon = player.GetComponentInChildren<Weapon>(true);
                 weapon.Damage += attackamount;
+                wall1.SetActive(false);
             }
 
             player.ResetHp();
