d7f9a0b7cf4e3b0b6a35b48751d4ee1eb9fe1efd
Onur Güler <guleronurosman@gmail.com>
2024-03-17 20:41:07 +0100

    power up floating text

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -11,6 +11,10 @@ public class PowerUp : MonoBehaviour
     private float speedamount = 4.5f;
     private int hpamount = 100;
     private int attackamount = 20;
+    public GameObject FloatingTextPrefab;
+    private bool _collected = false;
+    public Transform FloatingTextSpawnPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +24,14 @@ public class PowerUp : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (_collected)
+        {
+            return;
+        }
+
         if (other.CompareTag("Player"))
         {
+            _collected = true;
             player.ResetHp();
             if (id == 0)
             {
@@ -41,9 +51,44 @@ public class PowerUp : MonoBehaviour
                 weapon.MaxDamage += attackamount;
                 wall1.SetActive(false);
             }
-            gameObject.SetActive(false);
             player.powerUpsCollected++;
+
+            if (ShowFloatingText()) {
+                return;
+            }
+
+            DeActivate();
         }
+    }
+
+    bool ShowFloatingText() {
+        string text = GetFloatingText();
+        if (string.IsNullOrWhiteSpace(text)) {
+            return false;
+        }
+        GameObject floatingTextObj = Instantiate(FloatingTextPrefab, FloatingTextSpawnPos.position, Quaternion.identity, FloatingTextSpawnPos.parent);
+        floatingTextObj.transform.position = FloatingTextSpawnPos.position;
+        FloatingText floatingText = floatingTextObj.GetComponent<FloatingText>();
+        floatingText.Offset = Vector3.zero;
+        floatingText.RandomizeIntensity = Vector3.zero;
+        floatingText._textMeshProUGUI.fontSize /= 2;
+        floatingText.SetText(text);
+        floatingText.SetColor(Color.white);
+        Invoke(nameof(DeActivate), floatingText.DestroyTime);
+        return true;
+    }
+
+    void DeActivate() {
+        gameObject.SetActive(false);
+    }
+
+    string GetFloatingText() {
+        return id switch {
+            0 => "+Speed up",
+            1 => "+" + hpamount + " health",
+            2 => "+" + attackamount + " damage",
+            _ => string.Empty,
+        };
     }
 }
 
